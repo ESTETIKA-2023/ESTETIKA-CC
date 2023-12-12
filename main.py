45b06fbe52796005af41e739f7970603a5adf971
Deni Sikiandani <denisikiandani35@gmail.com>
2023-12-12 19:56:09 +0800

    update main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-cred = credentials.Certificate("tes-project-1-c64c0-firebase-adminsdk-ard2g-e66bf333bf.json")
+cred = credentials.Certificate("ServiceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -18,21 +18,107 @@ def find_batik_villages():
     processed_titles = set()
     processed_location = set()
 
+    location_jawa_timur = {
+        ("Desa Wisata Simbatan Keren, Kabupaten Magetan"),
+        ("Desa Wisata Budaya Sendang, Kabupaten Tulungagung"),
+        ("Desa Wisata Kertosari Pasuruan, Kabupaten Pasuruan"),
+        ("Desa Wisata Kampung Batik, Kabupaten Tuban"),
+        ("Desa Wisata Kemirigede, Kabupaten Blitar"),
+        ("Desa Wisata Duren Sari Sawahan, Kabupaten Trenggalek"),
+        ("Desa Wisata Wonoagung, Kabupaten Malang"),
+        ("Desa Wisata Simbatan Keren, Kabupaten Magetan"),
+    }
+
+    location_jawa_tengah = {
+        ("Desa Wisata Makukuhan Kembang Madu, Kabupaten Temanggung"),
+        ("Desa Wisata Karangturi, Kabupaten Rembang"),
+        ("Desa Wisata Kampoeng Djadhoel, Kota Semarang"),
+        ("Desa Wisata Bakaran Wetan, Kabupaten Pati"),
+        ("Desa Wisata Mangrove Pandansari Kaliwlingi, Kabupaten Brebes"),
+    }
+
+    location_yogyakarta = {
+        ("Desa Wisata Wukirsari, Kabupaten Bantul"),
+        ("Desa Wisata Wukirsari, Kabupaten Bantul"),
+        ("Desa Wisata Wukirsari, Kabupaten Bantul"),
+        ("Desa Wisata Putat, Kabupaten Gunung Kidul"),
+        ("Desa Wisata Jerukwudel, Kabupaten Gunung Kidul"),
+        ("Desa Wisata Wukirsari, Kabupaten Bantul"),
+        ("Desa Wisata Rejowinangun, Kota Yogyakarta"),
+        ("Desa Wisata DEWI GUMI, Kabupaten Bantul"),
+        ("Desa Wisata Hargorejo, Kabupaten Kulon Progo"),
+        ("Desa Wisata Wisata Wayang, Kabupaten Bantul"),
+        ("Desa Wisata Wisata Wayang, Kabupaten Bantul"),
+        ("Desa Wisata Nglanggeran, Kabupaten Gunung Kidul"),
+        ("Desa Wisata Nglanggeran, Kabupaten Gunung Kidul"),
+        ("Desa Wisata Wukirsari, Kabupaten Bantul"),
+    }
+
+    location_sumatera_utara = {
+        ("Desa Wisata Batik Sekar Najogi, Kabupaten Padang Lawas Utara"),
+    }
+
+    location_sumatera_selatan = {
+        ("Desa Wisata Beji Manik Karang, Kabupaten Ogan Komering Ulu Timur")
+    }
+
+    location_kalimantan_barat = {
+        ("Desa Wisata Kampong Melayu BML, Kota Pontianak"),
+    }
+
+    location_banten = {
+        ("Desa Wisata Curuggoong, Kabupaten Serang")
+    }
+
+    location_lampung = {
+        ("Desa Wisata Labuhan Ratu Vii, Kabupaten Lampung Timur")
+    }
+
+    def get_province(location_batik):
+        if location_batik in location_jawa_timur:
+            province = "Jawa Timur"
+            return province
+        elif location_batik in location_jawa_tengah:
+            province = "Jawa Tengah"
+            return province
+        elif location_batik in location_yogyakarta:
+            province = "Daerah Istimewa Yogyakarta"
+            return province
+        elif location_batik in location_sumatera_utara:
+            province = "Sumatra Utara"
+            return province
+        elif location_batik in location_sumatera_selatan:
+            province = "Sumatra Selatan"
+            return province
+        elif location_batik in location_kalimantan_barat:
+            province = "Kalimantan Barat"
+            return province
+        elif location_batik in location_banten:
+            province = "Banten"
+            return province
+        elif location_batik in location_lampung:
+            province = "Lampung"
+            return province
+        return "Unknown"
+        
     def saved_db():
         data = {
             "Nama Atraksi": title_atraksi,
             "Location": location_batik,
             "Price": price_abstraksi,
             "More Info": url_detail_village,
-            "Image": url_detail_image
+            "Image": url_detail_image,
+            "Province": province
             }
 
-        def save(collection_id, docuent_id, data):
-            db.collection(collection_id).document(docuent_id).set(data)
+        def save(collection_id, document_id, data):
+            db.collection(collection_id).document(document_id).set(data)
+
+        document_id = (f"{index}-{title_atraksi.replace(' ', '_').replace('[', '').replace(']', '')}")
 
         save(
-            collection_id= "Batik Villages",
-            docuent_id= (f"{index} | {title_atraksi}"),
+            collection_id= "batik_tourist_attractions",
+            document_id=document_id,
             data= data
         )
 
@@ -51,10 +137,12 @@ def find_batik_villages():
         time.sleep(1)
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        # # browser keliatan
+        # browser = p.chromium.launch(headless=False)
 
         # # biar bowser ga keliatan
-        # browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=True)
+
         page = browser.new_page()
         page.goto(url_batik_villages)
         # time.sleep(2)
@@ -87,21 +175,13 @@ def find_batik_villages():
 
                 url_detail_image = (f'https://jadesta.kemenparekraf.go.id/{image_batik}')
 
+                province = get_province(location_batik)
+
                 if title_atraksi not in processed_titles and word_batik in title_atraksi:
                     processed_titles.add(title_atraksi)
                     processed_location.add(location_batik)
 
                     saved_db()
-
-
-                        # print(index)
-                        # print(f'Nama Atraksi: {title_atraksi}')
-                        # print(f'Lcation: {location_batik}')
-                        # print(f'Harga: {price_abstraksi}')
-                        # print(f'More Info: {url_detail_village}')
-                        # print(f'Image: {url_detail_image}\n')
-                        
-                # to check jika kondisi judul nya sama tetapi lokasinya harus beda
                 elif title_atraksi in processed_titles and word_batik in title_atraksi and location_batik not in processed_location:
                     processed_location.add(location_batik)
                     saved_db()
